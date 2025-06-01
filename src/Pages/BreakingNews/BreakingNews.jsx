1b934bb5ea834efd4068ffa3c6dafefb7c5a8385
Methu-Dev <engmethu@gmail.com>
2025-06-02 01:01:25 +0600

    clear right side

--- src/Pages/BreakingNews/BreakingNews.jsx
+++ src/Pages/BreakingNews/BreakingNews.jsx
@@ -5,9 +5,9 @@ const BreakingNews = () => {
     <div className="flex bg-gray-400 mt-7">
       <button className="btn btn-secondary">Latest</button>
       <Marquee pauseOnHover={true} className="cursor-pointer" speed={100}>
-        I can be a React component, multiple React components, or just some
-        text.
+        The MERN stack is a popular full-stack development framework that combines four powerful technologies: MongoDB, Express.js, React.js, and Node.js. It allows developers to build modern web applications entirely using JavaScript, both on the client side and server side. React.js is used to create dynamic and responsive user interfaces, while Express.js and Node.js handle the backend logic and server-side operations. MongoDB, a NoSQL database, stores data in flexible JSON-like documents, making it easy to work with data in JavaScript environments. The biggest advantage of the MERN stack is its seamless integration and use of a single language throughout the application, which speeds up development and makes maintenance easier. It’s widely used for building social media platforms, e-commerce sites, real-time chat applications, and many other web projects. Overall, MERN provides a robust and efficient way to develop scalable, high-performance web applications.
       </Marquee>
+
     </div>
   );
 };
--- src/Pages/Shared/RightSideNavbar/RightSideNavbar.jsx
+++ src/Pages/Shared/RightSideNavbar/RightSideNavbar.jsx
@@ -1,11 +1,51 @@
-
+import { BsInstagram } from "react-icons/bs";
+import { FaTwitter } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { IoLogoGithub } from "react-icons/io5";
+import { MdOutlineFacebook } from "react-icons/md";
+import qzone1 from '../../../assets/qZone1.png';
+import qzone2 from '../../../assets/qZone2.png';
+import qzone3 from '../../../assets/qZone3.png';
 
 const RightSideNavbar = () => {
   return (
     <div>
-        <h1 className="text-2xl">right side navbar</h1>
+      <div className="p-4 space-y-4">
+      <h1 className="text-2xl font-bold">Login With</h1>
+      <button className="btn btn-outline w-full">
+        <FcGoogle />
+        Login with Google
+      </button>
+      <button className="btn btn-outline w-full">
+        <IoLogoGithub />
+        Login with Github
+      </button>
     </div>
-  )
-}
+    {/* find us */}
+    <div className="p-4 mb-6">
+       <h1 className="text-2xl mb-4 font-poppins font-semibold">Find Us On</h1>
+       <a className="flex items-center p-4 text-lg rounded-t-lg border" href="">
+        <MdOutlineFacebook className="mr-2.5"/>
+        Facebook
+       </a>
+       <a className="flex items-center p-4 text-lg border-x" href="">
+        <FaTwitter className="mr-2.5"/>
+        Twitter
+       </a>
+       <a className="flex items-center p-4 text-lg rounded-b-lg border" href="">
+        <BsInstagram className="mr-2.5"/>
+        Instagram
+       </a>
+    </div>
+    {/* Q zone */}
+    <div className="p-4 space-y-4">
+      <h1 className="text-2xl font-bold">Q-Zone</h1>
+      <img src={qzone1} alt="missing img" />
+      <img src={qzone2} alt="missing img" />
+      <img src={qzone3} alt="missing img" />
+    </div>
+    </div>
+  );
+};
 
-export default RightSideNavbar
+export default RightSideNavbar;
